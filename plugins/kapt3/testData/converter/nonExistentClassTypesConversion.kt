0c5d5c110ff6bc321ff26853e1ce8f5a23a2136d
Yan Zhulanow <Yan.Zhulanow@jetbrains.com>
2017-01-11 16:58:52 +0900

    Kapt3: Replace error/NonExistentClass with the actual type (from PSI) (KT-15421)

---
+++ plugins/kapt3/testData/converter/nonExistentClassTypesConversion.kt
@@ -0,0 +1,27 @@
+// NON_EXISTENT_CLASS
+// NO_VALIDATION
+
+@Suppress("UNRESOLVED_REFERENCE")
+class Test {
+    lateinit var a: ABC
+    val b: ABC? = null
+    val c: List<ABC>? = null
+    val d: List<Map<BCD, ABC<List<BCD>>>>? = null
+    lateinit var e: List<out Map<out ABC, out BCD>?>
+    lateinit var f: ABC<*>
+    lateinit var g: List<*>
+    lateinit var h: ABC<Int, String>
+    lateinit var i: (ABC, List<BCD>) -> CDE
+    lateinit var j: () -> CDE
+    lateinit var k: ABC.(List<BCD>) -> CDE
+
+    fun f1(a: ABC): BCD? {
+        return null
+    }
+
+    fun <T> f2(a: ABC<String, Int, () -> BCD>) {}
+
+    fun <T> f3(a: ABC, b: Int): Long {
+        return 0
+    }
+}
